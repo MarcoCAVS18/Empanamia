8ea73c23934771f7b49f8bc631b98f516a23e64e
MarcoCAVS99 <marcopiermatei1@gmail.com>
2023-06-21 23:04:30 +0200

    Actualizacion 21 06 23:04

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,28 +1,68 @@
+import React, { useState } from "react";
 import image from './Mesa de trabajo 2.svg';
 import CartWidget from '../CartWidget/CartWidget';
 import NavButtons from './NavButtons';
 
 function NavBar() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav className='h-24 bg-blue'>
-      <div className="max-w-8xl mx-auto px-8 sm:px-6 lg:px-8 flex items-center h-full">
-        <div className="flex items-center justify-start">
-          <img
-            className="h-20 w-auto"
-            src={image}
-            alt="Logo"
-          />
-          <h1 className='text-3xl font-sans font-semibold pl-4 select-none tracking-widest text-white'>Empanamía</h1>
-        </div>
-        <div className="flex items-center justify-end flex-grow">
-          <div className="flex items-center space-x-4">
+    <nav className="bg-blue">
+      <div className="max-w-8xl mx-auto px-8 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-24">
+          <div className="flex items-center">
+            <img
+              className="h-20 w-auto"
+              src={image}
+              alt="Logo"
+            />
+            <h1 className="text-3xl font-sans font-semibold pl-4 select-none tracking-widest text-white">
+              Empanamía
+            </h1>
+          </div>
+          <div className="hidden md:flex items-center space-x-4">
             <NavButtons />
+            <CartWidget /> {/* Mueve el componente CartWidget aquí */}
+          </div>
+          <div className="md:hidden flex items-center">
+            <CartWidget /> {/* Mueve el componente CartWidget aquí */}
+            <button
+              className="text-white focus:outline-none pl-6"
+              onClick={toggleMobileMenu}
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d={isMobileMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+                />
+              </svg>
+            </button>
           </div>
         </div>
-        <div className="md:hidden flex items-center">
-          <CartWidget />
-        </div>
+        {isMobileMenuOpen && (
+          <div className="md:hidden mt-4">
+            <div className="flex flex-col space-y-4">
+              <NavButtons />
+            </div>
+          </div>
+        )}
       </div>
+      {isMobileMenuOpen && (
+        <div className="md:hidden bg-gray-100">
+          {/* Remueve el componente CartWidget aquí */}
+        </div>
+      )}
     </nav>
   );
 }
--- src/components/NavBar/NavButtons.js
+++ src/components/NavBar/NavButtons.js
@@ -1,20 +1,18 @@
-import CartWidget from "../CartWidget/CartWidget";
+
 
 function NavButtons() {
   const buttonLabels = ["Todas", "Fritas", "Al horno", "Destacadas"];
 
   return (
-    <div className="ml-10 flex items-center space-x-6">
+    <div className="ml-10 flex flex-col md:flex-row items-center space-x-0 md:space-x-6 md:space-y-0 md:items-center md:space-y-0">
       {buttonLabels.map((label, index) => (
         <button
           key={index}
-          className="uppercase text-white font-medium px-6 py-3 rounded-lg bg-transparent hover:bg-pink"
+          className="uppercase text-white font-medium px-6 py-3 rounded-lg bg-transparent hover:bg-pink md:inline-block md:px-6 md:py-3"
         >
           {label}
         </button>
       ))}
-      <span className="h-6 w-px bg-white"></span>
-      <CartWidget />
     </div>
   );
 }
