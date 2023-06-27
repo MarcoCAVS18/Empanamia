b03ecbe067ba522bb8cabb7a266974c93469e8a5
MarcoCAVS99 <marcopiermatei1@gmail.com>
2023-06-27 13:47:27 +0200

    Nueva actualizacion 27 06 2023

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,7 +4,7 @@ const ItemList = ({ products }) => {
   return (
     <div className="flex justify-center items-center gap-6 flex-wrap">
       {products.map((product) => (
-        <div className="md:w-1/4 lg:w-1/6" key={product.id}>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6" key={product.id}>
           <Item {...product} />
         </div>
       ))}
